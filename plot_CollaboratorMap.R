1a97534a5a0d57135cc0e74ef0410f370010443c
Patrick Savage <patsavagenz@gmail.com>
2026-03-26 13:26:34 +1300

    Update plot_CollaboratorMap.R

--- plot_CollaboratorMap.R
+++ plot_CollaboratorMap.R
@@ -113,7 +113,7 @@ gobj <- ggplot(data = world) + theme_set(theme_bw()) +
   theme(panel.background = element_rect(fill = "aliceblue")) + 
   scale_fill_manual(values = LANGCOLORMAP$rgb, breaks = LANGCOLORMAP$languagefamily)
 
-##Change it yo your own outputdir 
+##Change it your own output directory 
 OUTPUTDIR = "/Users/betty/Downloads/"
 ggsave(file = paste(OUTPUTDIR, "CollabMap.png", sep = ""), plot = gobj, width = 8, height = 7, dpi = 600)
 write.csv(file = paste(OUTPUTDIR, "langlabel.csv", sep = ""), langlabel)
